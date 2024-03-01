5675c1377645441ca31044079582687dd1d0353c
miiiingi <kimmingi722@gmail.com>
2024-03-01 18:56:37 +0900

    refactor: 코드 정리

--- src/main/java/com/sparta/fifth_week_lv3/entity/Admin.java
+++ src/main/java/com/sparta/fifth_week_lv3/entity/Admin.java
@@ -17,6 +17,7 @@ public class Admin {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "유효한 이메일 주소를 입력하세요.")
     @NotBlank
     private String email;
@@ -24,6 +25,7 @@ public class Admin {
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,15}$", message = "비밀번호는 최소 8자 이상, 15자 이하이며 알파벳 대소문자, 숫자, 특수문자를 포함해야 합니다.")
     @NotBlank
     private String password;
+
     @Enumerated(EnumType.STRING)
     private DepartmentTypeEnum department;
 
