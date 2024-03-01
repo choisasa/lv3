f117dd2df537305630cc991bc8700d850fb0f681
miiiingi <kimmingi722@gmail.com>
2024-03-01 12:27:07 +0900

    init commit

---
+++ src/main/java/com/sparta/fifth_week_lv3/FifthWeekLv3Application.java
@@ -0,0 +1,13 @@
+package com.sparta.fifth_week_lv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifthWeekLv3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FifthWeekLv3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/fifth_week_lv3/FifthWeekLv3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.fifth_week_lv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifthWeekLv3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
