e16eabe2e967e4917dbe6dc60d5847b937b106f3
miiiingi <kimmingi722@gmail.com>
2024-03-01 16:21:50 +0900

    fix: 관리자 등록 에러시 발생하는 에러 다룰 수 있도록 추가

--- src/main/java/com/sparta/fifth_week_lv3/controller/AdminController.java
+++ src/main/java/com/sparta/fifth_week_lv3/controller/AdminController.java
@@ -6,14 +6,21 @@ import com.sparta.fifth_week_lv3.service.AdminService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "관리자 API", description = "관리자 등록")
+import java.util.List;
+
+@Slf4j(topic = "관리자 등록 및 로그인")
+@Tag(name = "관리자 API", description = "관리자 등록 및 로그인")
 @RestController
 @RequiredArgsConstructor
 public class AdminController {
@@ -23,9 +30,22 @@ public class AdminController {
     @Operation(summary = "관리자 등록", description = "관리자 등록시 사용할 정보를 입력합니다.")
     @Parameter(name = "JSON", description = "email, password, department, authority 필요")
     @PostMapping("/admin")
-    public ResponseEntity<AdminResponseDto> createAdmin(@RequestBody AdminRequestDto requestDto) {
-        AdminResponseDto responseDto = adminService.createAdmin(requestDto);
+    public ResponseEntity<?> createAdmin(@Valid @RequestBody AdminRequestDto requestDto, BindingResult bindingResult) {
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if (fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+        }
 
-        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+        try {
+            AdminResponseDto responseDto = adminService.createAdmin(requestDto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+        } catch (Exception e) {
+            log.error("관리자 등록 중 에러 발생: " + e.getMessage(), e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 에러 발생");
+        }
     }
+
+
 }
